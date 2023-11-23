5bc87aa7749dcb07b9294a906592acc64dde579d
Benjamin Martin <ben.w.martin17@gmail.com>
2023-11-22 20:13:24 -0500

    refactoring functions

--- js/main.js
+++ js/main.js
@@ -19,68 +19,50 @@ function renderCoffees(coffees) {
     return html;
 }
 
-function updateCoffeesAdd(e, input) {
-    e.preventDefault();
-    let filteredCoffees = [];
-        input.forEach(coffee => {
-                filteredCoffees.push(coffee);
-        });
+//--------sorts search params changes to be rendered------
+function updateCoffees(filteredCoffees) {
+
+
     coffeeDiv.innerHTML = renderCoffees(filteredCoffees.sort(coffees.id).reverse());
 }
 
-function updateCoffeesSearch(e, input) {
+//---------updates displayed list by roast----------------
+function updateRoast(e) {
     e.preventDefault();
     const selectedRoast = roastSelection.value;
     let filteredCoffees = [];
-    if (nameSelector.value === "" && roastSelection.value === "all")  {
+    if (selectedRoast === "all") {
         filteredCoffees = [...coffees];
-                console.log("no input, roast all");
-    }
-    if (nameSelector.value === "" && roastSelection.value !== "all") {
-        for (let i = 0; i < coffees.length; i++) {
-            if (coffees[i].roast === roastSelection.value) {
-                filteredCoffees.push(coffees[i]);
-                console.log("no input, roast selected");
-            }
-        }
-    } else if (input !== undefined && roastSelection.value === "all") {
-        filteredCoffees = [...input];
-        console.log("input defined, roast all")
-    } else if (input !== undefined && roastSelection.value !== "all") {
-        input.forEach(coffee => {
-            if (coffee.roast === selectedRoast) {
-                filteredCoffees.push(coffee);
-                console.log("input defined, roast selected")
-            }
-        });
-    }
-    coffeeDiv.innerHTML = renderCoffees(filteredCoffees.sort(coffees.id).reverse());
-}
-
-function updateInput(e) {
-    e.preventDefault();
-    const nameSelectorNew = nameSelector.value.toLowerCase()
-    let filteredCoffees = [];
-    if (nameSelectorNew === "") {
-        coffees.forEach(coffee => {
-            filteredCoffees.push(coffee);
-        })
     } else {
         coffees.forEach(coffee => {
-                if (coffee.name.toLowerCase().includes(nameSelectorNew)) {
-                    filteredCoffees.push(coffee);
-                }
+            if (coffee.roast === selectedRoast) {
+                filteredCoffees.push(coffee);
             }
-        )
+        })
     }
-    updateCoffeesSearch(e, filteredCoffees);
+    updateCoffees(filteredCoffees);
 }
 
-function addCoffee(e, roast, name) {
+// --------updates displayed list according to name--------
+function updateName(e) {
     e.preventDefault();
-    name = document.querySelector("#add-name-selector").value.toLowerCase();
+    const nameSelectionNew = nameSelection.value.toLowerCase()
+    let filteredCoffees = [];
+    coffees.forEach(coffee => {
+            if (coffee.name.toLowerCase().includes(nameSelectionNew)) {
+                filteredCoffees.push(coffee);
+            }
+        }
+    )
+    updateCoffees(filteredCoffees);
+}
+
+// ------------adds new coffee to list--------------------
+function createNewCoffee(e) {
+    e.preventDefault();
+    let name = document.querySelector("#add-name-selector").value.toLowerCase();
     name = name.charAt(0).toUpperCase() + name.slice(1);
-    roast = document.querySelector("#add-roast-selection").value;
+    let roast = document.querySelector("#add-roast-selection").value;
     if (roast !== "all" && name !== "") {
         const newCoffee = {
             id: coffees.length + 1,
@@ -88,7 +70,9 @@ function addCoffee(e, roast, name) {
             roast: roast
         }
         coffees.unshift(newCoffee);
-        updateCoffeesAdd(e, coffees);
+        coffeeDiv.innerHTML = renderCoffees(coffees.sort(coffees.id).reverse());
+    } else {
+        alert("Roast and name required.");
     }
     document.querySelector("#add-name-selector").value = "";
 }
@@ -113,18 +97,13 @@ const coffees = [
 
 const coffeeDiv = document.querySelector('#coffees');
 const roastSelection = document.querySelector('#roast-selection');
-const roastSelectionAdd = document.querySelector('#add-roast-selection');
-const nameSelector = document.querySelector('#name-selector');
-const nameSelectorAdd = document.querySelector('#add-name-selector');
+const nameSelection = document.querySelector('#name-selector');
 const addBtn = document.querySelector("#add button");
-let roast = "";
-let name = "";
-document.getElementById("add").addEventListener('submit', addCoffee, false)
-
+document.getElementById("add").addEventListener('submit', createNewCoffee, false)
 
 coffeeDiv.innerHTML = renderCoffees(coffees);
 
-roastSelection.addEventListener("change", updateCoffeesSearch);
-nameSelector.addEventListener("input", updateInput);
-addBtn.addEventListener("click", addCoffee);
+roastSelection.addEventListener("change", updateRoast);
+nameSelection.addEventListener("input", updateName);
+addBtn.addEventListener("click", createNewCoffee);
 
